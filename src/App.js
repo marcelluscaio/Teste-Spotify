3e8215e2c8804c5f27a14aea942191c0933896af
Caio Cabral <ocaiocabral@gmail.com>
2022-11-30 12:01:23 -0400

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+const client_id = 'a01ac38bd9514c2eb5cf5e9acb2c9fa1';
+const client_secret = 'ab22cec87e72492e9a2b5e5fdc0071d4';
+
+
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [input, setInput] = useState('');
+  useEffect(() => {
+    let authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+  } , [])
+
+  async function search(){
+    console.log("Searching for " + input)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type={'text'} placeholder={'Your text here'} value={input} onChange={(e) => {setInput(e.target.value)}}/>
+        <button onClick={(e) => {
+          e.preventDefault();
+          search()}}>Click me!</button>
+      </form>
+      
     </div>
   );
 }
